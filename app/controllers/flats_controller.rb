029cce52053797b1a87d47f83c79b8241e8ed5e6
alexis zaffalon <alexiszaffalon@live.fr>
2020-12-20 16:33:09 +0100

    add bootstrap

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,14 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create
+    @flat.save
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting DB...'
+Flat.destroy_all
+
+puts 'Creating new Flats...'
+
+London = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Javea = Flat.create!(
+  name: 'CASA DEL SOL. Sunny modern home w.WIFI in Old Town',
+  address: 'Javea',
+  description: 'The place is everything you need for a sweet and comfortable experience.',
+  price_per_night: 90,
+  number_of_guests: 4
+)
+
+Montpellier = Flat.create!(
+  name: 'Duplex sous toiture à Montpellier rives du lez',
+  address: 'Montpellier',
+  description: 'Un superbe studio duplex pour vous entre les quartier Antigone Rives du lez !!',
+  price_per_night: 35,
+  number_of_guests: 2
+)
+
+Barcelone = Flat.create!(
+  name: 'Feel at home abroad-Family friendly',
+  address: 'Barcelone',
+  description: 'Lovely apartment located in the heart of Barcelona, right next to all city attractions and hotspots within walking distance (Aquarium, Montjuïc Fountains & Castle, las Ramblas, Red District), 50 meters from the metro station Poble Sec.',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+[London, Javea, Montpellier, Barcelone].each do |flat|
+  puts "#{flat.name} created!"
+end
+
+puts "All Flats created!"
